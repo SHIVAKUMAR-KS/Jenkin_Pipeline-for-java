e0dae1d714de21eaa8f0cd1ace05cd4b1e836428
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-09-18 18:04:28 +0530

    upodated

--- main.java
+++ main.java
@@ -1,7 +1,9 @@
 public class main {
     public static void main(String[] args) {
+        System.out.println("********************************");
         System.out.println("Hello, World!");
         System.out.println("This is a test");
+        System.out.println("********************************");
     }
     
 }
